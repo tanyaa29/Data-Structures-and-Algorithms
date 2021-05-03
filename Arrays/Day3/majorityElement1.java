4505c3879c7c90658da11c2f93e2eaaaed922264
Tanya <tanyachapahadi@gmail.com>
2021-05-03 15:56:26 +0530

    day3prog3

---
+++ Arrays/Day3/majorityElement1.java
@@ -0,0 +1,16 @@
+public class Solution {
+    public int majorityElement(int[] nums) {
+        int cnt = 0;
+        int ele = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (cnt == 0)
+                ele = nums[i];
+            if (nums[i] == ele)
+                cnt++;
+            else
+                cnt--;
+        }
+        return ele;
+
+    }
+}
