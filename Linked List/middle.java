0cf33b04ad481fe741959be8f129fd19a0bad839
Tanya <tanyachapahadi@gmail.com>
2021-05-05 14:59:49 +0530

    day5prog2

---
+++ Linked List/middle.java
@@ -0,0 +1,12 @@
+public class middle {
+    public ListNode middleNode(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+
+    }
+}
