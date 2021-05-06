3dc0d95c39cdb13a1df96499559519485a3445b3
Tanya <tanyachapahadi@gmail.com>
2021-05-07 00:05:29 +0530

    day6prog4

---
+++ Linked List/posofcycle.java
@@ -0,0 +1,25 @@
+public class posofcycle {
+   
+        public ListNode detectCycle(ListNode head) {
+            if (head == null || head.next == null)
+                return null;
+            ListNode slow = head;
+            ListNode fast = head;
+            ListNode enter = head;
+            while (fast.next != null && fast.next.next != null) {
+
+                slow = slow.next;
+                fast = fast.next.next;
+                if (slow == fast) {
+                    while (slow != enter) {
+                        slow = slow.next;
+                        enter = enter.next;
+                    }
+                    return enter;
+                }
+            }
+            return null;
+
+        }
+    
+}
