42e8973b29564ede876487d027e83348c28a56d1
Tanya <tanyachapahadi@gmail.com>
2021-05-29 18:14:42 +0530

    day18prog3

---
+++ Trees/Day2/height of binary tree/recursive.java
@@ -0,0 +1,11 @@
+
+
+public class recursive {
+    public int maxDepth(TreeNode root) {
+        if(root==null){
+            return 0;
+        }
+        return 1+Math.max(maxDepth(root.left),maxDepth(root.right));
+    }
+
+}
