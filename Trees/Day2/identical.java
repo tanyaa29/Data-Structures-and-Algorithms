ae5b258537ac549ed136700e059396d87b622ce0
Tanya <tanyachapahadi@gmail.com>
2021-05-31 00:52:41 +0530

    day18prog8

---
+++ Trees/Day2/identical.java
@@ -0,0 +1,15 @@
+boolean identicalTrees(Node a, Node b) 
+    {
+        /*1. both empty */
+        if (a == null && b == null)
+            return true;
+              
+        /* 2. both non-empty -> compare them */
+        if (a != null && b != null) 
+            return (a.data == b.data
+                    && identicalTrees(a.left, b.left)
+                    && identicalTrees(a.right, b.right));
+   
+        /* 3. one empty, one not -> false */
+        return false;
+    }
